05411895fa739694de1a1f3e68cf36324d8a3820
Kent Bridgeman <bridg109@gmail.com>
2021-10-21 19:51:11 -0500

    added download_dataset function
    Download_dataset is used to download datasets from a provided url.
    It saves them to a specified folder.
    Uses a steam so any file size can be downloaded.

--- dags/sagerx.py
+++ dags/sagerx.py
@@ -1,3 +1,4 @@
+# Filesystem functions
 def create_path(*args):
     """creates and returns folder path object if it does not exist"""
     from pathlib import Path
@@ -6,3 +7,23 @@ def create_path(*args):
     if not p.exists():
         p.mkdir(parents=True)
     return p
+
+
+# Web functions
+def download_dataset(url: str, dest: os.PathLike = Path.cwd(), file_name: str = None):
+    """Downloads a data set file from provided Url via a requests steam
+    url = url to send request to
+    dest = path to save downloaded file to
+    filename = name to call file, if None last segement of url is used"""
+    import requests
+
+    if file_name == None:
+        dest_path = create_path(dest) / url.split("/")[-1]
+    else:
+        dest_path = create_path(dest) / file_name
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(dest_path, "wb") as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+    return dest_path

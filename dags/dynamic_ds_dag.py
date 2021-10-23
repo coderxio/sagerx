a7ea0e55b51863d49b9e81314c2a143b4cde5fb3
Kent Bridgeman <bridg109@gmail.com>
2021-10-22 21:52:52 -0500

    added fda_excluded data set
    Added dict for fda_excluded to dynamic dag
    Put SQL in fda_exlcuded dag folder
    This will download and load fda_excluded into DB

--- dags/dynamic_ds_dag.py
+++ dags/dynamic_ds_dag.py
@@ -11,8 +11,14 @@ data_set_list = [
         "schedule": "0 6 * * 5",  # run a 6am every thur (url marco minuses day to get wed)
         "url": "https://download.medicaid.gov/data/nadac-national-average-drug-acquisition-cost-{{ macros.ds_format( macros.ds_add(ds ,-1), '%Y-%m-%d', '%m-%d-%Y' ) }}.csv",
         #   "url": "https://download.medicaid.gov/data/nadac-national-average-drug-acquisition-cost-10-20-2021.csv"
-    }
+    },
+    {
+        "dag_id": "fda_excluded",
+        "schedule": "30 4 * * *",  # run a 4:30am every day
+        "url": "https://www.accessdata.fda.gov/cder/ndc_excluded.zip",
+    },
 ]
+
 
 ########################### DYNAMIC DAG DO NOT TOUCH BELOW HERE #################################
 

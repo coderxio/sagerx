73524330f6b98f741dcdbc7de75fe5d8ade6794c
Kent Bridgeman <bridg109@gmail.com>
2021-10-23 22:34:22 -0500

    added sql to load cms_ndc_hcpcs data set
    add sql files to load the 4 ds files from cms_ndc_hcpcs.

---
+++ dags/cms_ndc_hcpcs/load-opps_ndc_hcpcs.sql
@@ -0,0 +1,21 @@
+/* datasource.opps_ndc_hcpcs */
+DROP TABLE IF EXISTS datasource.opps_ndc_hcpcs;
+
+CREATE TABLE datasource.opps_ndc_hcpcs (
+
+hcpcs               TEXT,
+short_description   TEXT,
+labeler_name        TEXT,
+ndc                 TEXT,
+drug_name           TEXT,
+hcpcs_dosage        TEXT,
+pkg_size            TEXT,
+pkg_qty             TEXT,
+billing_units       TEXT,
+bill_units_pkg      TEXT
+);
+
+COPY datasource.opps_ndc_hcpcs
+FROM PROGRAM 'ds_path=$(find {{ ti.xcom_pull(key='file_path',task_ids='get_cms_ndc_hcpcs') }}/ -name "*OPPS NDC-HCPCS*.csv")
+				tail -n +9 "$ds_path"'
+CSV HEADER ENCODING 'ISO-8859-1';

6ce17e75601036233e9613dbe5eae0e7914db865
Kent Bridgeman <bridg109@gmail.com>
2021-11-08 15:06:29 -0600

    added ds_datetime
    This function specifically takes airflow marco {{ ds }}
    and converts it to a datetime object. Otherwise
    airflow defulat template for dates all return strings

--- dags/user_macros.py
+++ dags/user_macros.py
@@ -1,10 +1,11 @@
-from datetime import date, timedelta
+from datetime import datetime, date, timedelta
 import calendar
 
 
 def get_date_of_prior_weekday(
     weekday, reference_date=date.today(), date_format="%m-%d-%Y"
 ):
+
     """Gets the date of the prior weekday.  If today is the same weekday, returns today's date."""
     weekday = weekday.lower()
     weekdays = [d.lower() for d in list(calendar.day_name)]
@@ -18,3 +19,7 @@ def get_date_of_prior_weekday(
     prior_weekday = prior_weekday.strftime(date_format)
 
     return prior_weekday
+
+
+def ds_datetime(ds):
+    return datetime.strptime(ds, "%Y-%m-%d")

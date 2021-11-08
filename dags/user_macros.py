3ebff4aa506a480bb0cde0ab78b6eacbc5bdf5e2
Kent Bridgeman <bridg109@gmail.com>
2021-11-08 13:55:19 -0600

    renamed arument date to reference_date

--- dags/user_macros.py
+++ dags/user_macros.py
@@ -2,7 +2,9 @@ from datetime import date, timedelta
 import calendar
 
 
-def get_date_of_prior_weekday(weekday, start_date=date.today(), date_format="%m-%d-%Y"):
+def get_date_of_prior_weekday(
+    weekday, reference_date=date.today(), date_format="%m-%d-%Y"
+):
     """Gets the date of the prior weekday.  If today is the same weekday, returns today's date."""
     weekday = weekday.lower()
     weekdays = [d.lower() for d in list(calendar.day_name)]
@@ -11,8 +13,8 @@ def get_date_of_prior_weekday(weekday, start_date=date.today(), date_format="%m-
     if weekday_number < 0:
         raise ValueError
 
-    offset = (start_date.weekday() - weekday_number) % 7
-    prior_weekday = start_date - timedelta(days=offset)
+    offset = (reference_date.weekday() - weekday_number) % 7
+    prior_weekday = reference_date - timedelta(days=offset)
     prior_weekday = prior_weekday.strftime(date_format)
 
     return prior_weekday

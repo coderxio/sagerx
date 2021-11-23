30a81a33cc31fdfcf4d716789c461967a945d18d
Kent Bridgeman <bridg109@gmail.com>
2021-11-22 22:33:08 -0600

    Add sql to create ndac view
    SQL creates nadac in flatfiles

---
+++ dags/flat_files/export-nadac.sql
@@ -0,0 +1,25 @@
+/* flatfile.nadac*/
+CREATE OR REPLACE VIEW flatfile.nadac
+AS 
+   SELECT ndc
+         ,price_line
+   		,nadac_per_unit
+   		,pricing_unit 
+   		,price_start_date
+         ,CASE WHEN price_line = 1 THEN 'Y' END AS current_flag
+         ,CASE WHEN price_line = FIRST_VALUE(price_line) OVER (PARTITION BY ndc ORDER BY price_line DESC) THEN 'Y' END AS first_price
+         ,(nadac_per_unit - LAG(nadac_per_unit) OVER (PARTITION BY ndc ORDER BY price_line DESC)) AS dollar_change
+         ,(nadac_per_unit - LAG(nadac_per_unit) OVER (PARTITION BY ndc ORDER BY price_line DESC))/
+         					LAG(nadac_per_unit) OVER (PARTITION BY ndc ORDER BY price_line DESC) AS percent_change
+         ,CASE WHEN (nadac_per_unit - LAG(NADAC_Per_Unit) OVER (PARTITION BY ndc ORDER BY price_line DESC)) > 0 THEN 1
+         	   WHEN (nadac_per_unit - LAG(NADAC_Per_Unit) OVER (PARTITION BY ndc ORDER BY price_line DESC)) = 0 THEN 0
+         	   WHEN (nadac_per_Unit - LAG(NADAC_Per_Unit) OVER (PARTITION BY ndc ORDER BY price_line DESC)) IS NULL THEN NULL
+         	   ELSE -1 END AS change_type
+		
+   FROM staging.nadac nadac
+   WHERE nadac.price_line = 1;
+
+
+
+
+   

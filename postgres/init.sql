e521229beaaa95196f280484860394209c885a8e
Kent Bridgeman <bridg109@gmail.com>
2021-11-26 16:42:23 -0600

    Update postgres init sql for fdw
    Adds foreign data wrapper to postgres init. So
    That sagerx can pull data from airflow database

--- postgres/init.sql
+++ postgres/init.sql
@@ -2,6 +2,13 @@ CREATE USER airflow WITH ENCRYPTED PASSWORD 'airflow';
 CREATE DATABASE airflow;
 GRANT ALL PRIVILEGES ON DATABASE airflow TO airflow;
 
+--Make schemas for sagerx
 CREATE SCHEMA staging;
 CREATE SCHEMA datasource;
 CREATE SCHEMA flatfile;
+
+--Make foreign data wrapper to allow sagerx read access to airflow tables
+CREATE EXTENSION IF NOT EXISTS postgres_fdw;
+CREATE SERVER airflow_fdw FOREIGN DATA WRAPPER postgres_fdw OPTIONS (host 'postgres', port '5432', dbname 'airflow');
+CREATE USER MAPPING FOR sagerx SERVER airflow_fdw OPTIONS (user 'airflow', password 'airflow');
+GRANT USAGE ON FOREIGN SERVER airflow_fdw TO sagerx;

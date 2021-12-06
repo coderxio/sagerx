09d524585c298ec9fa914e75e29def340b6e4807
Joey LeGrand <jrlegrand@gmail.com>
2021-12-06 21:35:47 +0000

    Remove name and tty from ndc table

--- dags/rxnorm_full/staging-rxnorm_ndc.sql
+++ dags/rxnorm_full/staging-rxnorm_ndc.sql
@@ -4,25 +4,15 @@ DROP TABLE IF EXISTS staging.rxnorm_ndc CASCADE;
 CREATE TABLE staging.rxnorm_ndc (
     ndc        			 varchar(12) PRIMARY KEY,
     clinical_rxcui       varchar(8),
-    clinical_tty         varchar(20),
-    clinical_str   		 TEXT,
-    brand_rxcui          varchar(8),
-	brand_tty			 varchar(20),
-    brand_str        	 TEXT
+    brand_rxcui          varchar(8)
 );
 
 INSERT INTO staging.rxnorm_ndc
 SELECT rxnsat.atv as ndc
-	,CASE WHEN product.tty IN ('BPCK','SBD') THEN clinical_product.rxcui 
-		ELSE rxnsat.rxcui END AS clinical_product_rxcui
-	,CASE WHEN product.tty IN ('BPCK','SBD') THEN clinical_product.tty
-		ELSE product.tty END AS clinical_product_tty
-	,CASE WHEN product.tty IN ('BPCK','SBD') THEN clinical_product.str
-		ELSE product.str END AS clinical_product_name
-		
-	,CASE WHEN product.tty IN ('BPCK','SBD') THEN rxnsat.rxcui ELSE NULL END AS brand_product_rxcui
-	,CASE WHEN product.tty IN ('BPCK','SBD') THEN product.tty ELSE NULL END AS brand_product_tty
-	,CASE WHEN product.tty IN ('BPCK','SBD') THEN product.str ELSE NULL END AS brand_product_name
+	,CASE WHEN product.tty IN ('BPCK','SBD') THEN clinical_product.rxcui
+		ELSE rxnsat.rxcui END AS clinical_product_rxcui		
+	,CASE WHEN product.tty IN ('BPCK','SBD') THEN rxnsat.rxcui
+		ELSE NULL END AS brand_product_rxcui
 
 FROM datasource.rxnorm_rxnsat rxnsat
 	INNER JOIN datasource.rxnorm_rxnconso product ON rxnsat.rxaui = product.rxaui

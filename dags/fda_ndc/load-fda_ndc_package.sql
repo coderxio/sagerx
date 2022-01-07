a8ca7d4e29e0b830afeeddd35cae2818b3492776
Kent Bridgeman <bridg109@gmail.com>
2022-01-07 14:36:25 -0600

    Fix fda loads
    Fix load scripts to remove the Header and
    not add null values

--- dags/fda_ndc/load-fda_ndc_package.sql
+++ dags/fda_ndc/load-fda_ndc_package.sql
@@ -13,4 +13,4 @@ sample_package      TEXT
 );
 
 COPY datasource.fda_ndc_package
-FROM '{{ ti.xcom_pull(key='file_path',task_ids='get_fda_ndc') }}/package.txt' WITH (DELIMITER E'\t', NULL '', ENCODING 'WIN1252');
+FROM '{{ ti.xcom_pull(key='file_path',task_ids='get_fda_ndc') }}/package.txt' DELIMITER E'\t' CSV HEADER ENCODING 'WIN1252';;

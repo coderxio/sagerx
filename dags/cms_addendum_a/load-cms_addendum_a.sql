431dff57f6e5a874dbc813fa45c6373c07e1332a
Kent Bridgeman <bridg109@gmail.com>
2022-02-10 21:39:16 -0600

    Fix CMS Addendum A URL
    Updated CMS Addendum A Url due to change in website

--- dags/cms_addendum_a/load-cms_addendum_a.sql
+++ dags/cms_addendum_a/load-cms_addendum_a.sql
@@ -10,12 +10,9 @@ nation_unadjusted_copay        TEXT,
 min_unadjusted_copay           TEXT,
 note_column                    TEXT,
 pass_through_expiration_year   TEXT,
-effective_date                 TEXT,
-activation_date                TEXT,
-termination_date               TEXT,
 change_flag                    TEXT
 );
 COPY datasource.cms_addendum_a
-FROM PROGRAM 'ds_path=$(find {{ ti.xcom_pull(key='file_path',task_ids='get_cms_addendum_a') }}/ -name "*Addendum_A*.csv")
-                tail -n +3 "$ds_path"'
-CSV HEADER ENCODING 'ISO-8859-1';
+FROM PROGRAM 'ds_path=$(find {{ ti.xcom_pull(key='file_path',task_ids='get_cms_addendum_a') }}/ -name "*Addendum A.txt")
+                tail -n +2 "$ds_path"'
+CSV HEADER DELIMITER E'\t' ENCODING 'ISO-8859-1';

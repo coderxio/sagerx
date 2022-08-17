a301185607dcc0bb3a76bee2110c0bbc06192699
Kent Bridgeman <bridg109@gmail.com>
2022-08-17 15:16:41 -0500

    Update Nadac API
    Updated fields in NADAC API

--- dags/nadac/api-nadac.sql
+++ dags/nadac/api-nadac.sql
@@ -1,5 +1,18 @@
 /* flatfile.nadac*/
 TRUNCATE TABLE public.nadac;
 INSERT INTO public.nadac
-SELECT ndc || price_line::text AS ID,*
-FROM flatfile.nadac;
+SELECT ndc || price_line::text AS ID
+        ,ndc_description
+        ,price_line
+   		,nadac_per_unit
+   		,pricing_unit 
+   		,price_start_date
+        ,current_flag
+        ,first_price
+        ,CASE WHEN dollar_change IS NULL THEN 0 ELSE dollar_change END AS dollar_change
+        ,CASE WHEN percent_change IS NULL THEN 0 ELSE ROUND(percent_change * 100,2) END AS percent_change
+        ,CASE WHEN fn.change_type = -1 THEN 'decrease'
+              WHEN fn.change_type = 1 THEN 'increase'
+              ELSE 'none' END AS change_type
+
+FROM flatfile.nadac fn;

15ea6ab9c609b5a526946ae60ae65be388ae044d
Joey LeGrand <jrlegrand@gmail.com>
2022-11-30 02:20:36 +0000

    OCD capitalization

--- dags/rxnorm_full/view-rxnorm_ndc_to_product.sql
+++ dags/rxnorm_full/view-rxnorm_ndc_to_product.sql
@@ -1,14 +1,14 @@
 /* flatfile.rxnorm_ndc_to_product */
 CREATE OR REPLACE VIEW flatfile.rxnorm_ndc_to_product
 AS 
-    select
+    SELECT
         ndc
-        , coalesce(rbp.rxcui, rcp.rxcui, null) as rxcui
-        , coalesce(rbp.name, rcp.name, null) as name
-        , coalesce(rbp.tty, rcp.tty, null) as tty
-    from staging.rxnorm_ndc ndc
-    left join staging.rxnorm_clinical_product rcp 
-        on ndc.clinical_product_rxcui = rcp.rxcui
-    left join staging.rxnorm_brand_product rbp
-        on ndc.brand_product_rxcui = rbp.rxcui
-    where ndc.active and ndc.prescribable
+        , coalesce(rbp.rxcui, rcp.rxcui, null) AS rxcui
+        , coalesce(rbp.name, rcp.name, null) AS name
+        , coalesce(rbp.tty, rcp.tty, null) AS tty
+    FROM staging.rxnorm_ndc ndc
+    LEFT JOIN staging.rxnorm_clinical_product rcp 
+        ON ndc.clinical_product_rxcui = rcp.rxcui
+    LEFT JOIN staging.rxnorm_brand_product rbp
+        ON ndc.brand_product_rxcui = rbp.rxcui
+    WHERE ndc.active AND ndc.prescribable

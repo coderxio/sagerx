3c03693d7ca4966423e94032fd2cf181a5f76b1b
Joey LeGrand <jrlegrand@gmail.com>
2022-11-30 03:06:21 +0000

    Update ndc to product to include clinical

--- dags/rxnorm_full/view-rxnorm_ndc_to_product.sql
+++ dags/rxnorm_full/view-rxnorm_ndc_to_product.sql
@@ -1,11 +1,14 @@
 /* flatfile.rxnorm_ndc_to_product */
 CREATE OR REPLACE VIEW flatfile.rxnorm_ndc_to_product
 AS 
-    SELECT
+    SELECT DISTINCT
         ndc
-        , coalesce(rbp.rxcui, rcp.rxcui, null) AS rxcui
-        , coalesce(rbp.name, rcp.name, null) AS name
-        , coalesce(rbp.tty, rcp.tty, null) AS tty
+		, COALESCE(rbp.rxcui, rcp.rxcui, null) AS product_rxcui
+		, COALESCE(rbp.name, rcp.name, null) AS product_name
+		, COALESCE(rbp.tty, rcp.tty, null) AS product_tty
+		, rcp.rxcui AS clinical_product_rxcui
+		, rcp.name AS clinical_product_name
+		, rcp.tty AS clinical_product_tty
     FROM staging.rxnorm_ndc ndc
     LEFT JOIN staging.rxnorm_clinical_product rcp 
         ON ndc.clinical_product_rxcui = rcp.rxcui

87cfedd748ba535d348fbe6dade9c75b1407f8ad
Joey LeGrand <jrlegrand@gmail.com>
2023-01-04 12:49:09 +0000

    Create JSON staging table
    Create staging table with JSON SQL work. Rename staging SQL tables to regex and json. Pull in NDC11 and NDC9 in addition to application number in JSON table.

--- dags/fda_enforcement/staging-fda_enforement_openfda_ndc.sql
+++
@@ -1,4 +0,0 @@
-SELECT fdae.recall_number, arr.id_value #>> '{}' AS app_num
-
-FROM datasource.fda_enforcement fdae,
-	json_array_elements(openfda->'package_ndc') with ordinality arr(id_value, line);

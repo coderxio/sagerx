856e2eed0ad662d286137a52005e6ea7dfee8878
Joey LeGrand <jrlegrand@gmail.com>
2023-01-21 03:51:11 +0000

    Remove API tasks in DAG

--- dags/rxterms/api-rxterms_name.sql
+++
@@ -1,5 +0,0 @@
-/*  public.api_rxterms_name */
-TRUNCATE TABLE public.api_rxterms_name;
-INSERT INTO public.api_rxterms_name
-SELECT *
-FROM staging.rxterms_name;
--- dags/rxterms/api-rxterms_strength.sql
+++
@@ -1,5 +0,0 @@
-/*  public.api_rxterms_strength */
-TRUNCATE TABLE public.api_rxterms_strength;
-INSERT INTO public.api_rxterms_strength
-SELECT *
-FROM staging.rxterms_strength;

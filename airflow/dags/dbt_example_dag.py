bc80248db4184c3686f9dfa6092912045c4c23b7
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2023-01-29 16:56:44 -0600

    Example dbt dag
    Example dbt dag using the taskflow api. dbt commands are run using the
    airflow SubprocessHook. dbt run works as intended loading the example
    models.

---
+++ airflow/dags/dbt_example_dag.py
@@ -0,0 +1,29 @@
+from airflow.decorators.base import task_decorator_factory
+import pendulum
+
+from airflow.decorators import dag, task
+from airflow.hooks.subprocess import SubprocessHook
+
+
+@dag(
+    schedule=None,
+    start_date=pendulum.today(),
+    catchup=False,
+)
+def dbt_example_dag():
+
+    @task
+    def hello_world():
+        print('hello world')
+
+    @task
+    def execute_dbt():
+        subprocess = SubprocessHook()
+        subprocess.run_command(['dbt', 'run'], cwd='/dbt/sagerx_dbt')
+
+    hello_world = hello_world()
+    execute_dbt = execute_dbt()
+
+    hello_world >> execute_dbt
+
+dbt_example_dag()

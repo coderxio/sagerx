e430d27e097a3312a81ef8fb46c1f181f3d681d7
Joey LeGrand <jrlegrand@gmail.com>
2023-02-02 03:06:18 +0000

    Convert rxnorm_dose_form to dbt

---
+++ dbt/sagerx/models/staging/rxnorm/stg_rxnorm__dose_form.sql
@@ -0,0 +1,11 @@
+-- stg_rxnorm__dose_form.sql
+
+select
+	dose_form.rxcui rxcui
+	, dose_form.str name
+	, dose_form.tty tty
+	, case when dose_form.suppress = 'N' then true else false end as active
+	, case when dose_form.cvf = '4096' then true else false end as prescribable
+from datasource.rxnorm_rxnconso dose_form
+where dose_form.tty = 'DF'
+	and dose_form.sab = 'RXNORM'

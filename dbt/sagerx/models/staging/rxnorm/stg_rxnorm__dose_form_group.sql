9bf3a8df715ef9d909847df30801e5de2d26561a
Joey LeGrand <jrlegrand@gmail.com>
2023-02-02 12:48:13 +0000

    Convert dose form group to dbt

---
+++ dbt/sagerx/models/staging/rxnorm/stg_rxnorm__dose_form_group.sql
@@ -0,0 +1,11 @@
+-- stg_rxnorm__dose_form_group.sql
+
+select
+	dose_form_group.rxcui rxcui
+	, dose_form_group.str name
+	, dose_form_group.tty tty
+	, case when dose_form_group.suppress = 'N' then true else false end as active
+	, case when dose_form_group.cvf = '4096' then true else false end as prescribable
+from datasource.rxnorm_rxnconso dose_form_group
+where dose_form_group.tty = 'DFG'
+	and dose_form_group.sab = 'RXNORM'

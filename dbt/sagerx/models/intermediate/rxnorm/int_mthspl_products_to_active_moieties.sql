c70d72355bb723561950edfcdabe1955c154d0b2
Joey LeGrand <jrlegrand@gmail.com>
2023-02-16 22:42:14 +0000

    Convert mthspl prod to act moiety to dbt

---
+++ dbt/sagerx/models/intermediate/rxnorm/int_mthspl_products_to_active_moieties.sql
@@ -0,0 +1,34 @@
+-- int_mthspl_products_to_active_moieties.sql
+
+with
+
+substance as (
+
+    select * from {{ ref('stg_rxnorm__mthspl_substances') }}
+
+),
+
+product as (
+
+    select * from {{ ref('stg_rxnorm__mthspl_products') }}
+
+)
+
+select distinct
+    concat(lpad(split_part(product.ndc,'-', 1), 5, '0'), lpad(split_part(product.ndc,'-', 2), 4, '0')) as ndc9
+    , product.ndc as ndc
+    , product.rxcui as product_rxcui
+    , product.name as product_name
+    , product.tty as product_tty
+    , substance.unii as active_moiety_unii
+    , substance.rxcui as active_moiety_rxcui
+    , substance.name as active_moiety_name
+    , substance.tty as active_moiety_tty	
+    , product.active as active
+    , product.prescribable as prescribable
+from datasource.rxnorm_rxnrel rxnrel
+inner join substance
+    on rxnrel.rxaui1 = substance.rxaui
+inner join product
+    on rxnrel.rxaui2 = product.rxaui
+where rela = 'has_active_moiety'

3185bf0d9e78cb7bb2fdeb1ee14631445b93cd9a
Joey LeGrand <jrlegrand@gmail.com>
2023-04-24 19:58:59 +0000

    Add staging SQL for link and PIN

---
+++ dbt/sagerx/models/staging/rxnorm/stg_rxnorm__precise_ingredients.sql
@@ -0,0 +1,11 @@
+-- stg_rxnorm__precise_ingredients.sql
+
+select
+	ingredient.rxcui rxcui
+	, ingredient.str name
+	, ingredient.tty tty
+	, case when ingredient.suppress = 'N' then true else false end as active
+	, case when ingredient.cvf = '4096' then true else false end as prescribable
+from datasource.rxnorm_rxnconso ingredient
+where ingredient.tty = 'PIN'
+	and ingredient.sab = 'RXNORM'

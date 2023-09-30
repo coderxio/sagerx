24f318d0478135c665bacce6cf1bab8dbd9c0f0e
Joey LeGrand <jrlegrand@gmail.com>
2023-09-29 22:13:46 -0500

    Convert to dbt format

--- dbt/sagerx/models/intermediate/rxnorm/int_rxnorm_ingredients_to_routes.sql
+++ dbt/sagerx/models/intermediate/rxnorm/int_rxnorm_ingredients_to_routes.sql
@@ -1,18 +1,46 @@
+with 
+
+clinical_product_components as (
+	select * from {{ ref('stg_rxnorm__clinical_product_components') }}
+),
+
+ingredients as (
+	select * from {{ ref('stg_rxnorm__ingredients') }}
+),
+
+dose_forms as (
+	select * from {{ ref('stg_rxnorm__dose_forms') }}
+),
+
+dose_form_group_links as (
+	select * from {{ ref('stg_rxnorm__dose_form_group_links') }}
+),
+
+dose_form_groups as (
+	select * from {{ ref('stg_rxnorm__dose_form_groups') }}
+),
+
+dose_form_groups_to_routes as (
+	select * from {{ ref('rxnorm_dose_form_groups_to_routes') }}
+)
+
 select distinct
     ing.rxcui as ingredient_rxcui
 	, ing.name as ingredient_name
 	, route.route_name
 	, ing.active
 	, ing.prescribable
-from staging.stg_rxnorm__clinical_product_components prod
-left join staging.stg_rxnorm__ingredients ing
+from clinical_product_components prod
+left join ingredients ing
 	on ing.rxcui = prod.ingredient_rxcui
-left join staging.stg_rxnorm__dose_forms df
+left join dose_forms df
 	on df.rxcui = prod.dose_form_rxcui
-left join staging.stg_rxnorm__dose_form_group_links dfgl
+left join dose_form_group_links dfgl
 	on dfgl.dose_form_rxcui = df.rxcui
-left join staging.stg_rxnorm__dose_form_groups dfg
+left join dose_form_groups dfg
 	on dfg.rxcui = dfgl.dose_form_group_rxcui
-left join seeds.rxnorm_dose_form_group_to_route route
-	on route.dose_form_group_rxcui = dfg.rxcui
+left join dose_form_groups_to_routes route
+	-- NOTE: not sure why cast is needed since the column type
+	--       is defined in dbt_projet.yml
+	on cast(route.dose_form_group_rxcui as varchar) = dfg.rxcui
 where route.route_name is not null

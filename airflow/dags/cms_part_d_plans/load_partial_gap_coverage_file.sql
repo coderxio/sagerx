7b44493211500da1566f22c43317050638042fca
Joey LeGrand <jrlegrand@gmail.com>
2023-10-16 11:28:47 -0500

    Build DAG and SQL files

---
+++ airflow/dags/cms_part_d_plans/load_partial_gap_coverage_file.sql
@@ -0,0 +1,13 @@
+/* datasource.cms_partial_gap_coverage */
+DROP TABLE IF EXISTS datasource.cms_partial_gap_coverage CASCADE;
+
+CREATE TABLE datasource.cms_partial_gap_coverage (
+contract_id           VARCHAR(5) NOT NULL,
+plan_id         VARCHAR(3) NOT NULL,
+formulary_id      VARCHAR(8),
+rxcui  VARCHAR(8),
+contract_year  VARCHAR(8)
+);
+
+COPY datasource.cms_partial_gap_coverage
+FROM '{{ ti.xcom_pull(task_ids='extract') }}/partial gap coverage file  PPUF_{{params.year}}Q{{params.quarter}}.txt' DELIMITER '|' CSV HEADER;;
---
+++ airflow/dags/cms_part_d_plans/load_plan_information_file.sql
@@ -0,0 +1,23 @@
+/* datasource.cms_plan_information_file */
+DROP TABLE IF EXISTS datasource.cms_plan_information_file CASCADE;
+
+CREATE TABLE datasource.cms_plan_information_file (
+contract_id           VARCHAR(5) NOT NULL,
+plan_id         VARCHAR(3) NOT NULL,
+segment_id      VARCHAR(3),
+contract_name  VARCHAR(100),
+plan_name  VARCHAR(80),
+formulary_id    VARCHAR(8),
+premium    TEXT,
+deductible      TEXT,
+icl      TEXT,
+ma_region_code      VARCHAR(2),
+pdp_region_code      VARCHAR(2),
+state       VARCHAR(2),
+county_code       VARCHAR(5),
+snp       VARCHAR(1),
+plan_suppressed_yn       VARCHAR(1)
+);
+
+COPY datasource.cms_plan_information_file
+FROM '{{ ti.xcom_pull(task_ids='extract') }}/plan information  PPUF_{{params.year}}Q{{params.quarter}}.txt' DELIMITER '|' CSV HEADER ENCODING 'WIN1252';;

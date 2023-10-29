e957f8276553b650ee997991648e0081b3d3e868
Joey LeGrand <jrlegrand@gmail.com>
2023-10-28 23:11:36 -0500

    Cleanup ATC DAG

--- airflow/dags/rxnorm_historical/rxnorm_historical_dag.py
+++ airflow/dags/rxnorm_historical/rxnorm_historical_dag.py
@@ -3,6 +3,7 @@ import pandas as pd
 from time import sleep
 import requests
 from requests.adapters import HTTPAdapter, Retry
+import sqlalchemy
 
 from pathlib import Path
 import pendulum
@@ -22,8 +23,8 @@ from airflow.hooks.subprocess import SubprocessHook
     start_date=pendulum.yesterday(),
     catchup=False,
 )
-def rxclass_atc_to_product():
-    dag_id = "rxclass_atc_to_product"
+def rxnorm_historical():
+    dag_id = "rxnorm_historical"
     data_folder = Path("/opt/airflow/data") / dag_id
     file_name = f'{dag_id}.csv'
     file_path = create_path(data_folder) / file_name
@@ -36,7 +37,7 @@ def rxclass_atc_to_product():
         engine = pg_hook.get_sqlalchemy_engine()
 
         df = pd.read_sql(
-            "select distinct rxcui from datasource.rxnorm_rxnconso where tty in ('SCD','SBD','GPCK','BPCK') and sab = 'RXNORM' limit 100",
+            "select distinct rxcui from datasource.rxnorm_rxnconso where tty in ('SCD','SBD','GPCK','BPCK') and sab = 'RXNORM'",
             con=engine
         )
 
@@ -47,16 +48,16 @@ def rxclass_atc_to_product():
         
         return file_path_str
 
-    def get_atc_from_rxcui(rxcui):
+    def get_historical_ndcs(rxcui):
         try:
             s = requests.Session()
             retries = Retry(total=5,
                             backoff_factor=0.5)
             s.mount('https://', HTTPAdapter(max_retries=retries))
-            response = s.get(f'https://rxnav.nlm.nih.gov/REST/rxclass/class/byRxcui.json?rxcui={rxcui}&relaSource=ATCPROD')
+            response = s.get(f'https://rxnav.nlm.nih.gov/REST/rxcui/{rxcui}/allhistoricalndcs.json')
             response.raise_for_status()
-            jsonResponse = response.json()
-            resp = jsonResponse["rxclassDrugInfoList"]["rxclassDrugInfo"][0]["rxclassMinConceptItem"]
+            resp = response.json()
+            #resp = jsonResponse["rxclassDrugInfoList"]["rxclassDrugInfo"][0]["rxclassMinConceptItem"]
             return resp
         except Exception as e:
             print(e)
@@ -68,31 +69,36 @@ def rxclass_atc_to_product():
         data = pd.read_csv(file_path)
         rxcui_list = data['rxcui'].to_list()
 
-        # Get ATC for full list of RXCUI
-        atcs = {}
+        # Get historical NDCs for full list of RXCUI
+        ndcs = {}
         failed_atc = []
 
         for rxcui in rxcui_list:
             sleep(0.1)
-            atc = get_atc_from_rxcui(rxcui)
-            if atc == -1:
+            ndc = get_historical_ndcs(rxcui)
+            if ndc == -1:
                 failed_atc.append(rxcui)
             else:
-                atcs[rxcui] = atc
+                ndcs[rxcui] = ndc
 
-        atc_df = pd.DataFrame.from_dict(atcs, orient='index').reset_index()
-        print(atc_df.head(10))
+        ndc_df = pd.DataFrame.from_dict(ndcs, orient='index').reset_index()
+        ndc_df.columns = ['rxcui', 'ndcs']
+        print(ndc_df.head(10))
 
         pg_hook = PostgresHook(postgres_conn_id="postgres_default")
         engine = pg_hook.get_sqlalchemy_engine()
 
-        atc_df.to_sql(
-            "rxclass_atc_to_product",
+        ndc_df.to_sql(
+            "rxnorm_historical",
             con=engine,
             schema="datasource",
             if_exists="replace",
+            dtype={"ndcs": sqlalchemy.types.JSON},
             index=False
         )
+
+
+
 
     # Task to load data into source db schema
     load = []
@@ -115,7 +121,6 @@ def rxclass_atc_to_product():
         result = subprocess.run_command(['dbt', 'run', '--select', 'models/staging/fda_ndc'], cwd='/dbt/sagerx')
         print("Result from dbt:", result)
 
-    #extract() >> load >> transform()
     extract(get_rxcuis())
 
-rxclass_atc_to_product()
+rxnorm_historical()

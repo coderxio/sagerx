59f09a33100707fb548f9f55f5ab3a055ac0de42
Joey LeGrand <jrlegrand@gmail.com>
2023-11-08 11:47:10 -0600

    Create intermediate table to map RxNorm NDCs to RXCUIs

---
+++ dbt/sagerx/models/intermediate/rxnorm/int_rxnorm_all_ndcs_to_product_rxcuis.sql
@@ -0,0 +1,18 @@
+with all_ndcs as
+(
+    select * from {{ ref('stg_rxnorm__all_ndcs') }}
+),
+
+product_rxcuis as
+(
+    select * from datasource.rxnorm_rxnconso
+    where sab = 'RXNORM'
+        and tty in ('SCD', 'SBD', 'GPCK', 'BPCK')
+)
+
+select distinct
+    all_ndcs.ndc11
+    , product_rxcuis.rxcui
+from all_ndcs
+inner join product_rxcuis
+    on all_ndcs.rxcui = product_rxcuis.rxcui

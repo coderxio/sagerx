8785ff24d1ddd2c846abb0cc388f7b62a48a7971
Joey LeGrand <jrlegrand@gmail.com>
2023-11-29 16:31:43 -0600

    Initial Dailymed dbt work

---
+++ dbt/sagerx/models/staging/dailymed/stg_dailymed__ndcs.sql
@@ -0,0 +1,18 @@
+ /* staging.stg_dailymed__ndcs */
+
+with xml_table as
+(
+select spl, xml_content::xml as xml_column
+from datasource.dailymed_rx_full
+)
+
+select spl, y.*, ndc_to_11(y.ndc) as ndc11
+    from   xml_table x,
+            xmltable('dailymed/NDCList'
+              passing xml_column
+              columns 
+                document_id 	text  path '../documentId',
+				set_id  		text  path '../SetId',
+				version_number	 text  path '../VersionNumber',
+				ndc				text  path '.'
+					) y

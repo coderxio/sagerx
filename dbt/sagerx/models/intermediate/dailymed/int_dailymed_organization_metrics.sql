3d5e4268dbc974cd06869a5da01103560f6c4c33
Joey LeGrand <jrlegrand@gmail.com>
2023-11-29 21:19:57 -0600

    Convert organization metrics to intermediate model

---
+++ dbt/sagerx/models/intermediate/dailymed/int_dailymed_organization_metrics.sql
@@ -0,0 +1,18 @@
+/* intermediate.int_dailymed_organization_metrics */
+
+select o.set_id
+	, ma.market_status
+	, sum(case when org_type = 'Functioner' then 1 else 0 end) as functioner_count
+	, sum(case when org_type = 'Labeler' then 1 else 0 end) as labeler_count
+	, sum(case when org_type = 'Repacker' then 1 else 0 end) as repacker_count
+	, case when sum(case when ot.set_id is not null then 1 else 0 end) > 0 then 'Yes' else '' end as organization_text
+	, case when sum(case when org_type = 'Labeler' then 1 else 0 end) = 1 
+				and sum(case when org_type = 'Functioner' then 1 else 0 end) = 0
+			then 'yes' else '' end as labeler_only
+	, count(*)
+from staging.dailymed_organization o
+	inner join staging.dailymed_main ma
+        on o.set_id = ma.set_id
+	left join staging.dailymed_organization_text ot
+        on o.set_id = ot.set_id
+group by o.set_id, ma.market_status

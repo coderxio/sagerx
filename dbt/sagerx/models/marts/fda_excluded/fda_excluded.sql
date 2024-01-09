f4c0c3e965b77ed5610f1c7fce0e9c9327f42c36
Lukasz Przychodzien <lpprzychodzien@gmail.com>
2024-01-09 07:24:35 -0500

    fda_excluded

---
+++ dbt/sagerx/models/marts/fda_excluded/fda_excluded.sql
@@ -0,0 +1,28 @@
+SELECT
+	ndc_to_11(pack.ndcpackagecode) AS ndc11
+	, pack.productid
+	, pack.productndc
+	, producttypename
+	, proprietaryname
+	, proprietarynamesuffix
+	, nonproprietaryname
+	, dosageformname
+	, routename
+	, pack.startmarketingdate
+	, pack.endmarketingdate
+	, marketingcategoryname
+	, applicationnumber
+	, labelername
+	, substancename
+	, active_numerator_strength
+	, active_ingred_unit
+	, pharm_classes
+	, deaschedule
+	, pack.ndc_exclude_flag
+	, listing_record_certified_through
+	, ndcpackagecode
+	, packagedescription
+	, sample_package
+FROM {{source('fda_excluded','fda_excluded_package')}} AS pack
+LEFT JOIN {{source('fda_excluded','fda_excluded_product')}} AS prod
+	ON pack.productid = prod.productid;

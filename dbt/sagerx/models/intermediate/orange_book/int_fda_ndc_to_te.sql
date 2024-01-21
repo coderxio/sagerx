f087ea8701a4197e926f6058de4fa09edc77d3f6
Lukasz Przychodzien <lpprzychodzien@gmail.com>
2024-01-21 15:26:09 -0500

    int update

--- dbt/sagerx/models/intermediate/orange_book/int_fda_ndc_to_te.sql
+++ dbt/sagerx/models/intermediate/orange_book/int_fda_ndc_to_te.sql
@@ -5,7 +5,7 @@ WITH cte AS (
         , obp.te_code
         , COUNT(fda.ndc11) OVER( PARTITION BY fda.ndc11 ) AS num_te_codes
     FROM {{ source('orange_book', 'orange_book_products') }} AS obp
-    INNER JOIN {{ ref('staging.stg_fda_ndc__ndc') }} AS fda 
+    INNER JOIN {{ ref('stg_fda_ndc__ndcs') }} AS fda 
         ON concat(CASE WHEN obp.appl_type = 'A' THEN 'ANDA' ELSE 'NDA' END, obp.appl_no) = fda.applicationnumber
     GROUP BY fda.ndc11, obp.te_code
 )
@@ -15,7 +15,7 @@ SELECT
     , cte.te_code
     , LEFT(cte.te_code, 2) AS first_two_te_code
     , LEFT(cte.te_code, 1) AS first_one_te_code
-FROM {{ ref('staging.stg_fda_ndc__ndc') }} AS fda 
+FROM {{ ref('stg_fda_ndc__ndcs') }} AS fda 
 INNER JOIN cte 
     ON fda.ndc11 = cte.ndc11 
     AND cte.num_te_codes = 1

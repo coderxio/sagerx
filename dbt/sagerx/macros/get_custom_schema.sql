9860966f9b948577c9e7aa1a465aad42343fc23f
Joey LeGrand <joseph.legrand@vumc.org>
2024-02-06 13:34:20 -0600

    Change custom schema macro

--- dbt/sagerx/macros/get_custom_schema.sql
+++ dbt/sagerx/macros/get_custom_schema.sql
@@ -1,17 +1,6 @@
 -- get_custom_schema.sql
--- see https://docs.getdbt.com/docs/build/custom-schemas for original macro
+-- https://docs.getdbt.com/docs/build/custom-schemas
 
 {% macro generate_schema_name(custom_schema_name, node) -%}
-
-    {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
-
-        {{ default_schema }}
-
-    {%- else -%}
-
-        {{ custom_schema_name | trim }}
-
-    {%- endif -%}
-
+    {{ generate_schema_name_for_env(custom_schema_name, node) }}
 {%- endmacro %}

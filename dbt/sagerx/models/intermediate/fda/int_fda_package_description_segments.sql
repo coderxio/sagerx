20a5d3b888fd9d6832819795a73816f3741a3b6b
Joey LeGrand <jrlegrand@gmail.com>
2024-04-30 16:34:44 -0500

    Add RegEx magic

--- dbt/sagerx/models/intermediate/fda/int_fda_package_description_segments.sql
+++ dbt/sagerx/models/intermediate/fda/int_fda_package_description_segments.sql
@@ -1,18 +1,20 @@
+{{ config(materialized='table') }}
+
 with 
 
 all_fda as (
 	select ndc11, packagedescription
-	from sagerx_dev.stg_fda_ndc__ndcs
+	from {{ ref('stg_fda_ndc__ndcs') }}
 
 	union all
 
 	select ndc11, packagedescription
-	from sagerx_dev.stg_fda_excluded__ndcs
+	from {{ ref('stg_fda_excluded__ndcs') }}
 
 	union all
 
 	select ndc11, packagedescription
-	from sagerx_dev.stg_fda_unfinished__ndcs
+	from {{ ref('stg_fda_unfinished__ndcs') }}
 )
 
 select
@@ -26,6 +28,16 @@ from (
 	    , all_fda.packagedescription
 	    , s.token
         , s.ordinality
-	from all_fda, unnest(string_to_array(all_fda.packagedescription, '/')) with ordinality as s(token, ordinality)
+	from
+		all_fda
+		, unnest(
+			string_to_array(
+				regexp_replace(
+					all_fda.packagedescription
+					, '(?<!\*.*)\/'
+					, ' | '
+					, 'g')
+				, '|')
+			) with ordinality as s(token, ordinality)
 ) z
 order by ndc11, package_line

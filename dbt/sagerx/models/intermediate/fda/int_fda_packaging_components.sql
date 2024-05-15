0ef208fc2590762f4957496e8a87b1ed9e28bfd1
Joey LeGrand <jrlegrand@gmail.com>
2024-05-15 14:51:16 -0500

    Inner outer regex

--- dbt/sagerx/models/intermediate/fda/int_fda_packaging_components.sql
+++ dbt/sagerx/models/intermediate/fda/int_fda_packaging_components.sql
@@ -17,31 +17,47 @@ all_fda as (
 	from {{ ref('stg_fda_unfinished__ndcs') }}
 )
 
+, split_components as (
+	select
+		z.ndc11
+		, z.packagedescription
+		, z.ordinality as component_line
+		, trim(z.token) as component_text
+	from (
+		select distinct 
+			all_fda.ndc11
+			, all_fda.packagedescription
+			, s.token
+			, s.ordinality
+		from
+			all_fda
+			, unnest(
+				string_to_array(
+					regexp_replace(
+						all_fda.packagedescription
+						, '(?<!\*.*)\/'
+						, ' | '
+						, 'g')
+					, '|')
+				) with ordinality as s(token, ordinality)
+	) z
+	order by ndc11, component_line
+)
+
+, inner_outer_text as (
+	select
+		c.*
+		, trim(substring(component_text from '(.*) in ')) as inner_text
+		, trim(substring(component_text from ' in (.*?)(?:\(|$)')) as outer_text
+		, trim(substring(component_text from '\((.+)\)')) as outer_ndc
+		, ndc_to_11(trim(substring(component_text from '\((.+)\)'))) as outer_ndc11
+	from split_components c
+)
+
 select
-	z.ndc11
-    , z.packagedescription
-   	, z.ordinality as component_line
-	, trim(z.token) as component_text
-	, trim(substring(z.token from '(.*) in ')) as inner_text
-	, trim(substring(z.token from ' in (.*?)(?:\(|$)')) as outer_text
-	, trim(substring(z.token from '\((.+)\)')) as outer_ndc
-	, ndc_to_11(trim(substring(z.token from '\((.+)\)'))) as outer_ndc11
-from (
-    select distinct 
-        all_fda.ndc11
-	    , all_fda.packagedescription
-	    , s.token
-        , s.ordinality
-	from
-		all_fda
-		, unnest(
-			string_to_array(
-				regexp_replace(
-					all_fda.packagedescription
-					, '(?<!\*.*)\/'
-					, ' | '
-					, 'g')
-				, '|')
-			) with ordinality as s(token, ordinality)
-) z
-order by ndc11, component_line
+	*
+	, (regexp_match(inner_text, '^(\w+)\s(.*)'))[1] as inner_value
+	, (regexp_match(inner_text, '(\w+)\s(.*)'))[2] as inner_unit
+	, (regexp_match(outer_text, '^(\w+)\s(.*)'))[1] as outer_value
+	, (regexp_match(outer_text, '(\w+)\s(.*)'))[2] as outer_unit
+from inner_outer_text

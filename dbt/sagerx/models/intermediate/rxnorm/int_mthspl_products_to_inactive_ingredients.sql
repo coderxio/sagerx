da7459ed2754e12a150d1116d548806d78172c55
Joey LeGrand <jrlegrand@gmail.com>
2024-06-13 21:36:57 -0500

    UNII work

--- dbt/sagerx/models/intermediate/rxnorm/int_mthspl_products_to_inactive_ingredients.sql
+++ dbt/sagerx/models/intermediate/rxnorm/int_mthspl_products_to_inactive_ingredients.sql
@@ -3,15 +3,15 @@
 with
 
 substance as (
-
     select * from {{ ref('stg_rxnorm__mthspl_substances') }}
+)
 
-),
-
-product as (
-
+, product as (
     select * from {{ ref('stg_rxnorm__mthspl_products') }}
+)
 
+, unii_codes as (
+    select * from {{ ref('stg_fda_unii__unii_codes') }}
 )
 
 select distinct
@@ -21,6 +21,7 @@ select distinct
     , product.name as product_name
     , product.tty as product_tty
     , substance.unii as inactive_ingredient_unii
+    , unii_codes.display_name as inactive_ingredient_unii_display_name
     , substance.rxcui as inactive_ingredient_rxcui
     , substance.name as inactive_ingredient_name
     , substance.tty as inactive_ingredient_tty	
@@ -31,4 +32,15 @@ inner join substance
     on rxnrel.rxaui1 = substance.rxaui
 inner join product
     on rxnrel.rxaui2 = product.rxaui
+left join unii_codes rxcui_to_unii
+    on rxcui_to_unii.rxcui = substance.rxcui
+left join unii_codes
+    on unii_codes.unii = case
+        when (
+            substance.unii is not null 
+            and
+            substance.unii != 'NOCODE'
+        ) then substance.unii
+        else rxcui_to_unii.unii
+        end
 where rela = 'has_inactive_ingredient'
---
+++ dbt/sagerx/models/staging/fda_unii/stg_fda_unii__unii_codes.sql
@@ -0,0 +1,16 @@
+-- stg_fda_unii__unii_codes.sql
+
+with
+
+fda_unii as (
+    select * from {{ source('fda_unii', 'fda_unii') }}
+)
+
+select
+    unii
+    , display_name
+    , rxcui
+    , ncit
+    , ncbi
+    , dailymed
+from fda_unii

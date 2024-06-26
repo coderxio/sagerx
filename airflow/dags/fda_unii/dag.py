bf076c6080b51935e0ba705729f398106071c044
Joey LeGrand <jrlegrand@gmail.com>
2024-06-26 05:38:39 -0500

    UNII DAG work

--- airflow/dags/fda_unii/dag.py
+++ airflow/dags/fda_unii/dag.py
@@ -17,11 +17,21 @@ dag = create_dag(
 )
 
 with dag:
-    url= "https://precision.fda.gov/uniisearch/archive/latest/unii_data.zip"
+    '''
+    don't need this url because we are overriding the extract_task below
+    
+    this is required due to this issue: https://github.com/coderxio/sagerx/issues/276
+    '''
+    # url= "https://precision.fda.gov/uniisearch/archive/latest/unii_data.zip"
     ds_folder = get_ds_folder(dag_id)
 
+    '''
+    override result of extract_task, which is a data path
+
+    NOTE: if you need to update the file name, do so in load_unii.sql
+    '''
     # extract_task = extract(dag_id,url)
-    override_data_path = '/opt/airflow/data/fda_unii'
+    extract_task = '/opt/airflow/data/fda_unii'
     transform_task = transform(dag_id)
 
     sql_tasks = []
@@ -31,10 +41,13 @@ with dag:
         sql_task = PostgresOperator(
             task_id=task_id,
             postgres_conn_id="postgres_default",
-            sql=read_sql_file(sql_path).format(data_path=override_data_path),
+            sql=read_sql_file(sql_path).format(data_path=extract_task),
             dag=dag
         )
         sql_tasks.append(sql_task)
         
+    '''
+    don't need the extract_task for now
+    '''
     # extract_task >> sql_tasks >> transform_task
     sql_tasks >> transform_task

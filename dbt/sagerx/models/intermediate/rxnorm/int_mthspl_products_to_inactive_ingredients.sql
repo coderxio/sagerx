cb3e374a514430b2a59b5c2f10cefc9470dc063d
Joey LeGrand <jrlegrand@gmail.com>
2024-06-30 23:47:08 -0500

    Excipient model work
    Fixes #296

--- dbt/sagerx/models/intermediate/rxnorm/int_mthspl_products_to_inactive_ingredients.sql
+++ dbt/sagerx/models/intermediate/rxnorm/int_mthspl_products_to_inactive_ingredients.sql
@@ -1,4 +1,5 @@
 -- int_mthspl_products_to_inactive_ingredients.sql
+{{ config(materialized='table') }}
 
 with
 
@@ -10,10 +11,6 @@ substance as (
     select * from {{ ref('stg_rxnorm__mthspl_products') }}
 )
 
-, unii_codes as (
-    select * from {{ ref('stg_fda_unii__unii_codes') }}
-)
-
 select distinct
     concat(lpad(split_part(product.ndc,'-', 1), 5, '0'), lpad(split_part(product.ndc,'-', 2), 4, '0')) as ndc9
     , product.ndc as ndc
@@ -21,26 +18,14 @@ select distinct
     , product.name as product_name
     , product.tty as product_tty
     , substance.unii as inactive_ingredient_unii
-    , unii_codes.display_name as inactive_ingredient_unii_display_name
     , substance.rxcui as inactive_ingredient_rxcui
     , substance.name as inactive_ingredient_name
     , substance.tty as inactive_ingredient_tty	
     , product.active as active
     , product.prescribable as prescribable
-from sagerx_lake.rxnorm_rxnrel rxnrel
-inner join substance
-    on rxnrel.rxaui1 = substance.rxaui
-inner join product
+from product
+inner join sagerx_lake.rxnorm_rxnrel rxnrel
     on rxnrel.rxaui2 = product.rxaui
-left join unii_codes rxcui_to_unii
-    on rxcui_to_unii.rxcui = substance.rxcui
-left join unii_codes
-    on unii_codes.unii = case
-        when (
-            substance.unii is not null 
-            and
-            substance.unii != 'NOCODE'
-        ) then substance.unii
-        else rxcui_to_unii.unii
-        end
+inner join substance
+    on substance.rxaui = rxnrel.rxaui1
 where rela = 'has_inactive_ingredient'

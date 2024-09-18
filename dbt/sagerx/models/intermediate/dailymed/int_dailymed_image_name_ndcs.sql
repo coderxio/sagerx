236c3c6ed966907d4f4921e56307f65cd09648d5
Joey LeGrand <jrlegrand@gmail.com>
2024-09-18 00:28:55 +0000

    Add todo

--- dbt/sagerx/models/intermediate/dailymed/int_dailymed_image_name_ndcs.sql
+++ dbt/sagerx/models/intermediate/dailymed/int_dailymed_image_name_ndcs.sql
@@ -10,8 +10,20 @@ regex_ndcs as (
 
     select
         *,
-        (regexp_matches(image, '\d+-\d+-\d+', 'g'))[1] as ndc
+        (regexp_matches(image, '\d+-\d+(?:-\d+)?|d{11}|d{10}', 'g'))[1] as ndc
     from images
+
+    /*
+        \d{11}              | # 11 digit
+        \d{10}              | # 10 digit
+        \d{5}-\d{5}         | # 5-5
+        \d{5}-\d{4}-\d{2}   | # 5-4-2
+        \d{5}-\d{4}-\d{1}   | # 5-4-1
+        \d{5}-\d{3}-\d{2}   | # 5-3-2
+        \d{4}-\d{6}         | # 4-6
+        \d{4}-\d{4}-\d{2}     # 4-4-2
+
+    */
 
 ),
 
@@ -23,6 +35,8 @@ valid_spl_ndcs as (
 
 validated_ndcs as (
 
+    -- TODO: convert SPL NDCs to NDC11 and hyphenless and compare
+    -- to regex_ndc - but select the SPL version of the NDC
     select
         regex_ndcs.*
     from regex_ndcs

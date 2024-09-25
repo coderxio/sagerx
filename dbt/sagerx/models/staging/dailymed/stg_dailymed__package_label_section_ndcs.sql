fbc8f1d48a30a2f50bc0eec996b3715cef909a38
Joey LeGrand <jrlegrand@gmail.com>
2024-09-25 19:27:09 +0000

    Fix NDC whitespace issue

--- dbt/sagerx/models/staging/dailymed/stg_dailymed__package_label_section_ndcs.sql
+++ dbt/sagerx/models/staging/dailymed/stg_dailymed__package_label_section_ndcs.sql
@@ -12,7 +12,9 @@ ndcs as (
 	select
 		p.set_id,
 		p.id as package_label_section_id,
-		(regexp_matches(p.text, '\d+-\d+-\d+', 'g'))[1] as ndc
+		-- NOTE: removing whitespace with RegEx to account for line breaks and extra
+		-- whitespace padding betweeen NDC segments
+		(regexp_matches(regexp_replace(p.text, '\s', '', 'g'), '\d+-\d+-\d+', 'g'))[1] as ndc
 	from package_label_sections p
 
 ),

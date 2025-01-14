7b3273d590dfc5a3a43cef6b7b6aefb3fad96f9b
Joey LeGrand <jrlegrand@gmail.com>
2025-01-14 02:22:41 +0000

    Add headers to requests

--- airflow/dags/sagerx.py
+++ airflow/dags/sagerx.py
@@ -41,7 +41,11 @@ def download_dataset(url: str, dest: Path = Path.cwd(), file_name: str = None):
     import requests
     import re
 
-    with requests.get(url, stream=True, allow_redirects=True) as r:
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
+
+    with requests.get(url, stream=True, allow_redirects=True, headers=headers) as r:
         r.raise_for_status()
 
         if file_name == None:

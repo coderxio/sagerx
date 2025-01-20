bdb95dd1d634c5382e7e1a3a69f0d1cf73356ee8
Joey LeGrand <jrlegrand@gmail.com>
2025-01-20 22:40:31 +0000

    Update rxclass with data key

--- airflow/dags/rxclass/dag_tasks.py
+++ airflow/dags/rxclass/dag_tasks.py
@@ -18,22 +18,23 @@ def extract_rxclass(rxcui_list: list) -> None:
     # Collect all rows in a list to turn into a Dataframe later
     rxclass_rows = []
     for response in response_list:
-        if 'rxclassDrugInfoList' in response:
-            rxclasses = response['rxclassDrugInfoList']['rxclassDrugInfo']
-            for rxclass in rxclasses:
-                rxclass_row = {
-                    "rxcui": rxclass["minConcept"]["rxcui"],
-                    "name": rxclass["minConcept"]["name"],
-                    "tty": rxclass["minConcept"]["tty"],
-                    "class_id": rxclass["rxclassMinConceptItem"]["classId"],
-                    "class_name": rxclass["rxclassMinConceptItem"]["className"],
-                    "class_type": rxclass["rxclassMinConceptItem"]["classType"],
-                    "rela": rxclass["rela"],
-                    "rela_source": rxclass["relaSource"],
-                }
-                rxclass_rows.append(rxclass_row)
-        else:
-            print(f"Unexpected response format: {response}")
+        if not len(response['data']) == 0:
+            if 'rxclassDrugInfoList' in response['data']:
+                rxclasses = response['data']['rxclassDrugInfoList']['rxclassDrugInfo']
+                for rxclass in rxclasses:
+                    rxclass_row = {
+                        "rxcui": rxclass["minConcept"]["rxcui"],
+                        "name": rxclass["minConcept"]["name"],
+                        "tty": rxclass["minConcept"]["tty"],
+                        "class_id": rxclass["rxclassMinConceptItem"]["classId"],
+                        "class_name": rxclass["rxclassMinConceptItem"]["className"],
+                        "class_type": rxclass["rxclassMinConceptItem"]["classType"],
+                        "rela": rxclass["rela"],
+                        "rela_source": rxclass["relaSource"],
+                    }
+                    rxclass_rows.append(rxclass_row)
+            else:
+                print(f"Unexpected response format: {response}")
 
     # Create DataFrame from list of rows
     rxclass_df = pd.DataFrame(rxclass_rows)

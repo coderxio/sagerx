5e371acde280ffd1e30cb7642874a7df52eec61f
saywurdson <34385409+saywurdson@users.noreply.github.com>
2025-02-06 00:03:02 -0500

    created more intermediate models and a staging J code model

---
+++ dbt/sagerx/models/intermediate/rxclass/int_rxclass_clinical_products_to_atc_class.sql
@@ -0,0 +1,13 @@
+select distinct
+	rs.atv as clinical_product_ndc,
+	rcl.rxcui as clinical_product_rxcui,
+	rcl.name as clinical_product_name,
+	rcl.class_id as clinical_product_code,
+	rcl.class_name as clinical_product_class_name
+from sagerx_lake.rxnorm_rxnsat rs
+join sagerx_lake.rxnorm_rxnconso rc on rs.rxaui = rc.rxaui
+join sagerx_lake.rxclass rcl on rcl.rxcui = rs.rxcui
+where
+	rs.atn = 'NDC'
+	and rc.sab = 'RXNORM'
+	and rcl.rela_source = 'ATCPROD'

cc639ff367e3695063d1f5083185ccba57e262f5
saywurdson <34385409+saywurdson@users.noreply.github.com>
2025-03-16 20:16:53 -0400

    update dag to add transform_task to initiate the dbt transformations

--- airflow/dags/umls/dag.py
+++ airflow/dags/umls/dag.py
@@ -1,6 +1,7 @@
 import pendulum
 from airflow.decorators import dag
 from umls.dag_tasks import extract, load
+from common_dag_tasks import transform
 
 dag_id = "umls"
 
@@ -13,7 +14,8 @@ dag_id = "umls"
 def umls():
     extract_task = extract(dag_id)
     load_task = load(extract_task)
+    transform_task = transform(dag_id, models_subdir=['intermediate'])
 
-    extract_task >> load_task
+    extract_task >> load_task >> transform_task
 
 dag = umls()

531f8ed2db6af89b24374dc06a29b29a22b26e06
saywurdson <34385409+saywurdson@users.noreply.github.com>
2025-06-09 01:22:04 -0400

    finish atcddd code to create table and dbt models

--- dbt/sagerx/models/staging/atcddd/stg_atcddd__ingredient_to_ddd.sql
+++ dbt/sagerx/models/staging/atcddd/stg_atcddd__ingredient_to_ddd.sql
@@ -7,6 +7,6 @@ select
 	a.uom,
 	a.adm_route,
 	a.note
-from {{ source('atc_ddd', 'atc_ddd') }}  a 
+from {{ source('atc_ddd', 'atc_ddd') }} a 
 join {{ source('rxnorm_rxnconso', 'rxnorm_rxnconso') }} r on a.atc_code = r.code
 where r.sab = 'ATC'
